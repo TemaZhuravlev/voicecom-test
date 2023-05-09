d00e395f90b81432d1ca67826deebd9ec140435b
azhuravlev <azhuravlev@rtt.eu.com>
2023-05-09 21:40:30 +0200

    3.0

--- src/main/java/com/testvoicecom/managerclient/repository/ClientRepository.java
+++ src/main/java/com/testvoicecom/managerclient/repository/ClientRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
-
+    //получение всех клиентов
     @Query("SELECT c FROM Client c JOIN FETCH c.manager as m left join fetch m.alternate where c.enabled = true")
     List<Client> getAll();
 }
